bd822e6ddc4652924957f75f347ef3559a812481
Carlos Herrera <juan.cervantes@rappi.com>
2022-05-28 03:09:26 -0500

    EB-108 Implemented account reactivation

--- Attributes/IdentificationPartExistsAttribute.cs
+++ Attributes/IdentificationPartExistsAttribute.cs
@@ -9,41 +9,41 @@ namespace EscortBookCustomerProfile.Attributes
     public class IdentificationPartExistsAttribute : TypeFilterAttribute
     {
         public IdentificationPartExistsAttribute() : base(typeof(IdentificationPartExistsFilter)) {}
+    }
+    
+    internal class IdentificationPartExistsFilter : IAsyncActionFilter
+    {
+        private readonly IIdentificationPartRepository _identificationPartRepository;
 
-        private class IdentificationPartExistsFilter : IAsyncActionFilter
+        public IdentificationPartExistsFilter(IIdentificationPartRepository identificationPartRepository)
+            => _identificationPartRepository = identificationPartRepository;
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            private readonly IIdentificationPartRepository _identificationPartRepository;
-
-            public IdentificationPartExistsFilter(IIdentificationPartRepository identificationPartRepository)
-                => _identificationPartRepository = identificationPartRepository;
-
-            public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+            if (context.HttpContext.Request.HasFormContentType)
             {
-                if (context.HttpContext.Request.HasFormContentType)
-                {
-                    context.Result = new BadRequestResult();
-                    return;
-                }
-
-                var categoryId = context.HttpContext.Request.Form.Keys.FirstOrDefault(k => k == "identificationPartId") ??
-                    context.ActionArguments["identificationPartID"] as string;
-
-                if (categoryId is null)
-                {
-                    context.Result = new BadRequestResult();
-                    return;
-                }
-
-                var category = await _identificationPartRepository.GetByIdAsync(categoryId);
-
-                if (category is null)
-                {
-                    context.Result = new NotFoundResult();
-                    return;
-                }
-
-                await next();
+                context.Result = new BadRequestResult();
+                return;
             }
+
+            var categoryId = context.HttpContext.Request.Form.Keys.FirstOrDefault(k => k == "identificationPartId") ??
+                             context.ActionArguments["identificationPartID"] as string;
+
+            if (categoryId is null)
+            {
+                context.Result = new BadRequestResult();
+                return;
+            }
+
+            var category = await _identificationPartRepository.GetByIdAsync(categoryId);
+
+            if (category is null)
+            {
+                context.Result = new NotFoundResult();
+                return;
+            }
+
+            await next();
         }
     }
 }
